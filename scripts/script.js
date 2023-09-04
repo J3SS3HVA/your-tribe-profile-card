242c03a532d8e0517bc40740d63e7e4947d767ee
J3SS3HVA <jesse.leeuwerink@hva.nl>
2023-09-04 20:26:04 +0200

    version 1.3
    naam moet meer naar beneden

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const expandButton = document.querySelector('header button')
+const changepfp = document.querySelector('Img')
 expandButton.addEventListener('click', expand)
 
 function expand () {
   document.body.classList.toggle('expand')
 }
+
+
+
