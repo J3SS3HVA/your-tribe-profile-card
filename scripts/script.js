aa0aab5aca11dd2381db3d2b3132e5a20c1f76fe
J3SS3HVA <jesse.leeuwerink@hva.nl>
2023-09-18 12:53:23 +0200

    sphere

--- scripts/script.js
+++ scripts/script.js
@@ -6,5 +6,16 @@ function expand () {
   document.body.classList.toggle('expand')
 }
 
+// sphere follows mouse
+document.addEventListener('mousemove', (e) => {
+
+  const mouseFollow = document.getElementById('mouse-follow');
+  const x = e.clientX - 50; //-50 to center div over mouse
+  const y = e.clientY - 50; 
+  console.log(x);
+   
+  mouseFollow.style.top = `${y}px`;
+  mouseFollow.style.left = `${x}px`;
+})
 
 
